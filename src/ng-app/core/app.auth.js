ef2236a6e9f18f0b7a8831965e30d090962a9cf8
Kim Maida <git@kim-maida.com>
2015-05-04 10:30:29 -0400

    * Comments regarding state of redirection with Oauth services.

--- src/ng-app/core/app.auth.js
+++ src/ng-app/core/app.auth.js
@@ -9,10 +9,9 @@
 
 	function authRun($rootScope, $location, Fire) {
 		$rootScope.$on('$routeChangeStart', function(event, next, current) {
-			var _isAuthenticated = Fire.ref.getAuth();
+			var _isAuthenticated = !!Fire.ref.getAuth();
 
 			if (next && next.$$route) {
-
 				// if not authenticated, redirect to login page
 				// if possible, after login, redirect to intended route (large mq)
 				if (next.$$route.secure && !_isAuthenticated) {
@@ -25,6 +24,7 @@
 				}
 
 				// if attempting to access /login route and already logged in, redirect to homepage
+				// redirection to authPath only happens correctly with Google (not possible in some other Oauth services)
 				if (next.$$route.originalPath === '/login' && _isAuthenticated) {
 					$rootScope.$evalAsync(function() {
 						if ($rootScope.authPath) {
